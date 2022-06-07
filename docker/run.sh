7000d690ce668f8552535d2e5a71c01244777c85
shandawei <shandawei@me.com>
2022-06-07 10:31:49 +0800

    完善run脚本

--- docker/run.sh
+++ docker/run.sh
@@ -4,10 +4,12 @@
 docker build -t oapp-token-cron-task:default -f Dockerfile ..
 # 执行并配置参数
 docker run -d --restart always \
--v /root/github/oapp-token-cron-task/docker/config.yaml:/scripts/config.yaml
+-v /root/github/oapp-token-cron-task/docker/config.yaml:/scripts/config.yaml #! 挂载配置必须挂载
+# -v /root/github/oapp-token-cron-task/docker/logs:/root/.pm2/logs # 挂载日志方便查阅
 oapp-token-cron-task:default \
 
 # !第二种方式,直接通过打包好的公开镜像执行
 docker run -d --restart always \
--v /root/github/oapp-token-cron-task/docker/config.yaml:/scripts/config.yaml
+-v /root/github/oapp-token-cron-task/docker/config.yaml:/scripts/config.yaml  #! 挂载配置必须挂载
+# -v /root/github/oapp-token-cron-task/docker/logs:/root/.pm2/logs # 挂载日志方便查阅
 yuanzhibang/oapp-token-cron-task:default \
