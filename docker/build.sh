46e03687eee920e25cb8389810c50ffb8976f29e
shandawei <shandawei@me.com>
2022-06-07 10:26:28 +0800

    完善构建方法

--- docker/build.sh
+++ docker/build.sh
@@ -4,9 +4,9 @@ repo='yuanzhibang/oapp-token-cron-task'
 dockerFile='Dockerfile'
 
 docker build -t $repo:default -f $dockerFile ..
-# docker image tag $repo:default $repo:$time
-# docker image tag $repo:default $repo:latest
+docker image tag $repo:default $repo:$time
+docker image tag $repo:default $repo:latest
 
-# docker push $repo:default
-# docker push $repo:latest
-# docker push $repo:$time
+docker push $repo:default
+docker push $repo:latest
+docker push $repo:$time
