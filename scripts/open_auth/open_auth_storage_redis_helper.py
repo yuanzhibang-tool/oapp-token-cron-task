b44bbe111d0ae183e40c1f6160c67399dd40bce6
shandawei <shandawei@me.com>
2022-11-02 04:30:50 +0800

    add redis db support

--- scripts/open_auth/open_auth_storage_redis_helper.py
+++ scripts/open_auth/open_auth_storage_redis_helper.py
@@ -12,11 +12,12 @@ class OauthOpenAuthStorageRedisHelper(object):
         self.host = config['host']
         self.port = config['port']
         self.auth = config['auth']
+        self.db = config['db']
         self.client = redis.Redis(
-            host=self.host, port=self.port, password=self.auth, decode_responses=True)
+            host=self.host, port=self.port, password=self.auth, db=self.db, decode_responses=True)
 
     # 从数据库获取有效的server_access_token
-    def get_server_access_token(self,app_id):
+    def get_server_access_token(self, app_id):
         server_access_token_info_string = self.client.get(
             self.get_app_server_access_token_key(app_id))
         if server_access_token_info_string:
@@ -41,7 +42,7 @@ class OauthOpenAuthStorageRedisHelper(object):
                         save_dict_string, code_expires_in)
         # 从数据库获取有效的js_ticket
 
-    def get_js_ticket(self,app_id):
+    def get_js_ticket(self, app_id):
         js_ticket_info_string = self.client.get(
             self.get_app_js_ticket_key(app_id))
         if js_ticket_info_string:
@@ -51,7 +52,7 @@ class OauthOpenAuthStorageRedisHelper(object):
         return False
 
     # 保存js_ticket到数据库
-    def save_js_ticket(self,app_id, js_ticket_info):
+    def save_js_ticket(self, app_id, js_ticket_info):
         js_ticket = js_ticket_info['ticket']
         now_time = int(time.time())
         code_expires_in = js_ticket_info['expires_in']
@@ -66,10 +67,10 @@ class OauthOpenAuthStorageRedisHelper(object):
 
     # 从数据库获取即将失效的user_access_token
 
-    def get_app_server_access_token_key(self,app_id):
+    def get_app_server_access_token_key(self, app_id):
         return "type/server_access_token_info/app_id/%s" % app_id
 
-    def get_app_js_ticket_key(self,app_id):
+    def get_app_js_ticket_key(self, app_id):
         return "type/js_ticket_info/app_id/%s" % app_id
 
     def close_connection(self):
